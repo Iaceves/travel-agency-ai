33eb28a72dac88c8210fd77ed6c5a69b163c5fa5
Ismael <acevesaceves97@gmail.com>
2025-10-20 13:48:37 -0400

    implemented routing, dashboard & UI, Auth functionality

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Header } from '../../../components'
+
+function AllUsers() {
+
+  return (
+    <main className='dashboard wrapper'>
+        <Header
+            title="Trips Page"
+            description="check out our current users"
+        />
+        All Users Page Contents
+    </main>
+  )
+}
+
+export default AllUsers

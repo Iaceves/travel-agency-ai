a1d445d69c2b99a3f33f109a382b92ae304ae322
Ismael <acevesaceves97@gmail.com>
2025-11-16 20:02:30 -0500

    finish with trips page

--- app/routes/root/sign-in.tsx
+++ app/routes/root/sign-in.tsx
@@ -17,51 +17,6 @@ export const clientLoader = async () => {
   }
 };
 
-// const SignIn = () => {
-    
-//   return (
-//     <main className='auth'>
-//         <section className="size-full glassmorphism flex-center px-6">
-//             <div className="sign-in-card">
-//                 <header className="header">
-//                     <Link to="/">
-//                         <img 
-//                             src="/assets/icons/logo.svg"
-//                             alt="logo"
-//                             className='size-[30px]'
-//                         />
-//                     </Link>
-//                     <h1 className='p-28-bold'>
-//                         Travel Agency AI
-//                     </h1>
-//                     <article>
-//                         <h2 className='p-28-bold text-dark-100 text-center'>
-//                             Start Your Travel Journey
-//                         </h2>
-//                         <p className='p-18-regular text-center text-gray-100 !leading-7'>Sign in with Google to manage destination, itenerations, and user activity with ease</p>
-//                     </article>
-//                     <ButtonComponent
-//                         type="button"
-//                         iconCss="e-search-icon"
-//                         className='button-class !h-11 !w-full'
-//                         onClick={loginWithGoogle}
-//                     >
-//                         <img
-//                             src="/assets/icons/google.svg"
-//                             className='size-5'
-//                             alt="google"
-//                         />
-//                         <span className="p-18-semibold">Sign in with Google</span>
-//                     </ButtonComponent>
-//                 </header> 
-//             </div>
-//         </section>
-//     </main>
-//   )
-// }
-
-// export default SignIn
-
 
 const SignIn = () => {
   const navigate = useNavigate();

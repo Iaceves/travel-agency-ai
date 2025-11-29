49a3bc8b7d93ea5e719c86b998c3447198a12935
Ismael <acevesaceves97@gmail.com>
2025-11-28 20:39:56 -0500

    added m to left and rigth on page layout

--- app/routes/root/page-layout.tsx
+++ app/routes/root/page-layout.tsx
@@ -38,7 +38,7 @@ const pageLayout = () => {
                 <span className="p-16-semibold text-white">Go to Dashboard</span>
         </ButtonComponent>
     
-        <div>
+        <div className='ml-10 mr-10 grid grid-cols-1 lg:grid-cols-3 gap-5'>
             {allTrips.map(({id,name, imageUrls,itinerary,travelStyle,estimatedPrice}) => (
                 <TripCard 
                     key={id} 
